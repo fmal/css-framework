c28685cdcba955e76e10144436cc2f2cacc303ea
fmal <crimhead@gmail.com>
2013-10-21 15:57:22 +0200

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,114 @@
+module.exports = function(grunt) {
+    'use strict';
+
+    var paths, tasks;
+    // Configurable paths
+    paths = {
+        base       : '.',
+        styles     : '<%= path.base %>',
+        scss       : '<%= path.base %>/sass',
+    };
+
+    // Load tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    // Project configuration
+    grunt.initConfig({
+        path: paths,
+        pkg: grunt.file.readJSON('package.json'),
+
+        sass: {
+            compile: {
+                options: {
+                    style: 'expanded',
+                    unixNewlines: true,
+                    precision: 7,
+                    require: ['<%= path.scss %>/helpers/url64.rb']
+                },
+                expand: true,
+                cwd: '<%= path.scss %>',
+                src: ['**/!(_)*.scss'],
+                dest: '<%= path.styles %>',
+                ext: '.css'
+            }
+        },
+
+        autoprefixer: {
+            options: {
+                browsers: ['last 2 versions']
+            },
+            styles: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: '<%= path.styles %>',
+                        src: [
+                            '*.css',
+                            '!*.min.css',
+                            '!styles-old-ie.css'
+                        ],
+                        dest: '<%= path.styles %>'
+                    }
+                ]
+            },
+            stylesIE: {
+                options: {
+                    browsers: ['last 2 versions', 'ie 8', 'ie 7']
+                },
+                files: {
+                    '<%= path.styles %>/styles-old-ie.css': ['<%= path.styles %>/styles-old-ie.css']
+                }
+            }
+        },
+
+        csscomb: {
+            files: {
+                expand: true,
+                flatten: true,
+                src: [
+                    '<%= path.styles %>/*.css',
+                    '!<%= path.styles %>/*.min.css'
+                ],
+                dest: '<%= path.styles %>'
+            }
+        },
+
+        csso: {
+            options: {
+                report: 'gzip'
+            },
+            compress: {
+                options: {
+                    restructure: false
+                },
+                expand: true,
+                cwd: '<%= path.styles %>',
+                src: ['*.css', '!*.min.css', '!styles.css'],
+                dest: '<%= path.styles %>',
+                ext: '.min.css'
+            },
+            restructure: {
+                options: {
+                    restructure: true
+                },
+                src: '<%= path.styles %>/styles.css',
+                dest: '<%= path.styles %>/styles.min.css'
+            }
+        },
+
+        watch: {
+            sass: {
+                files: '<%= path.scss %>/**/*.scss',
+                tasks: 'default'
+            }
+        }
+    });
+
+    tasks = {
+        "default": ['sass', 'autoprefixer:styles', 'csscomb'],
+        release: ['sass', 'autoprefixer', 'csscomb', 'csso']
+    };
+
+    grunt.registerTask('default', tasks["default"]);
+    grunt.registerTask('release', tasks.release);
+};
---
+++ README.md
@@ -0,0 +1,13 @@
+My __CSS framework__ powered by SASS and based on scalable and maintainable CSS approaches.
+
+Inspired by:
+* inuit.css -- https://github.com/csswizardry/inuit.css by [@csswizardry](https://github.com/csswizardry)
+* Suit -- https://github.com/suitcss/suit by [@necolas](https://github.com/necolas)
+
+This framework depends on [SASS](http://sass-lang.com/) and [Bourbon library](https://bourbon.io/). To install run:
+
+    $ gem install sass
+    $ gem install bourbon
+    $ cd sass && bourbon install
+
+Use [Grunt](http://gruntjs.com/) tasks to compile CSS.
